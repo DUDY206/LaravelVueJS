bd2f82a1c21575fd9f0f5c68296b948aef0c268b
ninhkhuongduy206@gmail.com <ninhkhuongduy206@gmail.com>
2020-06-22 01:15:16 +0700

    auth by gate

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\AskQuestionRequest;
 use App\Question;
+use Illuminate\Auth\Access\Gate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -69,7 +70,12 @@ class QuestionController extends Controller
      */
     public function edit(Question $question)
     {
+
+        if(\Gate::denies('update-question',$question)){
+            abort(403,"Access denied");
+        }
         return view("questions.edit",compact('question'));
+
     }
 
     /**
@@ -93,6 +99,9 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
+        if(\Gate::denies('delete-question',$question)){
+            abort(403,"Access denied");
+        }
         $question->delete();
 
         return redirect('/questions')->with('success','Your question have been deleted');
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -34,12 +34,18 @@
                                     <div class="d-flex align-items-center">
                                         <h3 class="mt-0"><a href="{{$question->url}}">{{$question->title}}</a>
                                         <div class="ml-auto">
+                                            @if(Auth::user()->can('update-question',$question))
                                             <a href="{{route('questions.edit',$question->id)}}" class="btn btn-sm btn-outline-info">Edit</a>
-                                            <form class="form-delete" action="{{route('questions.destroy',$question->id)}}" method="post">
+                                            @endif
+
+                                                @if(Auth::user()->can('delete-question',$question))
+
+                                                <form class="form-delete" action="{{route('questions.destroy',$question->id)}}" method="post">
                                                 @method('DELETE')
                                                 @csrf
                                                 <input type="submit" class="btn btn-sm btn-outline-danger" value="Delete" ONCLICK="return confirm('Are you sure')">
-                                            </form>
+                                                </form>
+                                                @endif
                                         </div>
                                     </div>
 
