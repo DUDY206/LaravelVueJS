853fab1e7b57a83d345cc2c4570cef29a7a88b25
ninhkhuongduy206@gmail.com <ninhkhuongduy206@gmail.com>
2020-06-24 15:56:30 +0700

    Format show question and answer

---
+++ resources/js/font-awesome.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faSpinner, faCaretUp, faCaretDown, faStar } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faCaretUp, faCaretDown, faStar)
